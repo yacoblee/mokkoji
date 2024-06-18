6d33d5cc448e1d234bbaa0c2190b7c1bcfc1d7c1
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-18 20:20:06 +0900

    Create MyPageIndex.jsx

---
+++ src/components/mypage/MyPageIndex.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Header from './Routes/Header';
+
+function MyPageIndex() {
+
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                {/* <Header /> */}
+                <Routes>
+                    <Route ></Route>
+                    <Route ></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default MyPageIndex;
