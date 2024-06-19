7827eb98dbea1517e1b64fdd7238457855470f2d
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-06-20 01:19:36 +0900

    메인화면 수정

--- src/components/main/Productsection.jsx
+++ src/components/main/Productsection.jsx
@@ -5,10 +5,9 @@ const ProductSection = () => {
 
 
     return (
-
         <>
-            <img src="/images/main/traditional01.png" alt="" />
-
+            <img src="/images/main/traditional01.png" className='left_door' alt="" />
+            <img src="/images/main/traditional02.png" className='right_door' alt="" />
         </>
     );
 }
