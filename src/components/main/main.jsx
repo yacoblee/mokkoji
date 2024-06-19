56875da860b7822292c023ebd24ec847fed8a913
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-06-19 10:28:17 +0900

    메인 화면 수정
    메인화면 비디오 태그 수정
    비디오 태그 컴포넌트화

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,12 +6,8 @@ const Main = () => {
 
     return (
         <div className="master_page">
-
-            <Section id="section1" text="Section 1" color="#3c97d3b3" />
-            <Section id="section2" text="Section 2" color="#2ecc71" />
-            <Section id="section3" text="Section 3" color="#e74c3c" />
-            <Section id="section4" text="Section 4" color="#9b59b6" />
-            <Section id="section5" text="Section 5" color="#f39c12" />
+            <Section></Section>
+            
         </div>
     )
 }
