6f211471f3dcc2ece730a7198595c041df400dee
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-19 13:03:46 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './index.css';
-import Main from './components/main/main';
+import Main from './components/main/Main';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ProductContainer from './components/product/ProductContainer';
 import Header from './components/modules/Header';
@@ -10,23 +10,23 @@ import ProductDetails from './components/product/ProductDetails';
 
 
 const App = () => {
-    useEffect(() => {
-        const handleScroll = () => {
-            const header = document.getElementById('header');
-            if (window.scrollY > 10) {
-                header.classList.remove('deactive');
-                header.classList.add('active');
-            } else {
-                header.classList.remove('active');
-                header.classList.add('deactive');
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.getElementById('header');
+      if (window.scrollY > 10) {
+        header.classList.remove('deactive');
+        header.classList.add('active');
+      } else {
+        header.classList.remove('active');
+        header.classList.add('deactive');
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <BrowserRouter>
@@ -34,7 +34,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/goods/*" element={<ProductContainer />} />
-        <Route path='/goods/:category' element={<ProductList/>}></Route>
+        <Route path='/goods/:category' element={<ProductList />}></Route>
         <Route path="/goods/:category/:id" element={<ProductDetails />} />
         <Route path="/reserve/*" element={<div>Reserve Page</div>} />
         <Route path="/login/*" element={<div>Login Page</div>} />
