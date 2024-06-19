fc88697a87b9ecef2d0f06348682633f3de0d47a
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-19 15:10:07 +0900

    세부 구성

--- src/components/mypage/MyPageIndex.jsx
+++ src/components/mypage/MyPageIndex.jsx
@@ -7,19 +7,69 @@ function MyPageIndex() {
     return (
         <div>
             <header>여기는 헤더입니다 / 높이는 100px로 차지하고 있음</header>
+
             <h1>내 정보</h1>
-            <div class='MyPageIndex'>
-                <div class='MyPageMain'>
-                    <h2>@ 안녕하세요!</h2>
-                    <div class="MyOrder">주문 완료 건 : @건</div>
-                    <div class="MyInfo">내정보수정</div>
+
+            <div className='MyPageIndex'>
+                <div className='MyPageMain'>
+                    <div className='MyInfoGrid'>
+                        <div className="MyInfo">
+                            <h2>@ 안녕하세요!</h2>
+                            <div class="MyInfoSet">내정보수정</div>
+                        </div>
+
+                        <div className="MyOrder">
+                            <ul>
+                                <li>
+                                    <span>
+                                        <strong>대기</strong>
+                                    </span>
+                                    <span>
+                                        <a href="">=찜</a>
+                                    </span>
+                                </li>
+                                <li>
+                                    <span>
+                                        <strong>완료</strong>
+                                    </span>
+                                    <span>
+                                        <a href="">=배송</a>
+                                    </span>
+                                </li>
+                                <li>
+                                    <span>
+                                        <strong>확정</strong>
+                                    </span>
+                                    <span>
+                                        <a href="">=내역</a>
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class='MyGrid'>
-                    <div class='MyCart'>장바구니</div>
-                    <div class='MyLike'>찜목록</div>
-                    <div class='MyRecent'>최근 본 상품</div>
-                    <div class='MyList'>구매내역</div>
-                    <div class='MyPost'>배송 현황</div>
+
+                <div className='MyGrid'>
+                    <div className='MyLike'>
+                        <div><img src="" alt="찜" /></div>
+                        <div>찜목록</div>
+                    </div>
+                    <div className='MyCart'>
+                        <div><img src="" alt="장바구니" /></div>
+                        <div>장바구니</div>
+                    </div>
+                    <div className='MyPost'>
+                        <div><img src="" alt="배송" /></div>
+                        <div>배송 현황</div>
+                    </div>
+                    <div className='MyRecent'>
+                        <div><img src="" alt="기록" /></div>
+                        <div>최근 본 상품</div>
+                    </div>
+                    <div className='MyList'>
+                        <div><img src="" alt="내역" /></div>
+                        <div>구매내역</div>
+                    </div>
                 </div>
             </div>
         </div>
