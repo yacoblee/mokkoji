62b4ca8efdee2dbc1381cb888f93fe2863258f36
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-19 09:34:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './index.css';
-import Main from './components/main/Main';
+import Main from './components/main/main';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ProductContainer from './components/product/ProductContainer';
 import Header from './components/modules/Header';
@@ -8,35 +8,35 @@ import Header from './components/modules/Header';
 
 
 const App = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      const header = document.getElementById('header');
-      if (window.scrollY > 10) {
-        header.classList.remove('deactive');
-        header.classList.add('active');
-      } else {
-        header.classList.remove('active');
-        header.classList.add('deactive');
-      }
-    };
+    useEffect(() => {
+        const handleScroll = () => {
+            const header = document.getElementById('header');
+            if (window.scrollY > 10) {
+                header.classList.remove('deactive');
+                header.classList.add('active');
+            } else {
+                header.classList.remove('active');
+                header.classList.add('deactive');
+            }
+        };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/goods/*" element={<ProductContainer />} />
-        <Route path="/reserve/*" element={<div>Reserve Page</div>} />
-        <Route path="/login/*" element={<div>Login Page</div>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/goods/*" element={<ProductContainer />} />
+                <Route path="/reserve/*" element={<div>Reserve Page</div>} />
+                <Route path="/login/*" element={<div>Login Page</div>} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
