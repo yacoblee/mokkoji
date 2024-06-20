46576aa995375c65d1a5e93fc356a4c66d6d43ba
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-06-20 15:50:53 +0900

    메인 상품리스트 화면 이벤트
    메인화면 상품 영역  보여지는 이벤트 추가

--- src/components/main/Productsection.jsx
+++ src/components/main/Productsection.jsx
@@ -1,23 +1,52 @@
-import '../../css/main.css'
+import '../../css/main.css';
 import { useEffect } from 'react';
+
 const ProductSection = () => {
-    useEffect = () => {
+    useEffect(() => {
+        const sections = document.querySelectorAll('#section2');
+        const options = {
+            threshold: 0.8,
+        };
 
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                const leftImage = entry.target.querySelector('.left_door');
+                const rightImage = entry.target.querySelector('.right_door');
+                const startImage = entry.target.querySelector('.start_door');
+                const endImage = entry.target.querySelector('.end_door');
+                if (entry.isIntersecting) {
+                    leftImage?.classList.add('left_motion');
+                    rightImage?.classList.add('right_motion');
+                    startImage?.classList.add('start_motion');
+                    endImage?.classList.add('end_motion');
+                } else {
+                    leftImage?.classList.remove('left_motion');
+                    rightImage?.classList.remove('right_motion');
+                    startImage?.classList.remove('start_motion');
+                    endImage?.classList.remove('end_motion');
+                }
+            });
+        }, options);
 
+        sections.forEach((section) => {
+            observer.observe(section);
+        });
 
-
-
-
-
-    }
-
+        return () => {
+            sections.forEach((section) => {
+                observer.unobserve(section);
+            });
+        };
+    }, []);
 
     return (
-        <>
+        <div id="section2">
             <img src="/images/main/traditional01.png" className='left_door' alt="" />
+            <img src="/images/main/traditional01.png" className='start_door' alt="" />
+            <img src="/images/main/traditional01.png" className='end_door' alt="" />
             <img src="/images/main/traditional02.png" className='right_door' alt="" />
-        </>
+        </div>
     );
-}
+};
 
-export default ProductSection
+export default ProductSection;
