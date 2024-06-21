b0f1cdfca9c6cca7cc23590d3784c544f350a688
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-21 16:07:41 +0900

    .

--- src/components/mypage/MyPageMain.jsx
+++ src/components/mypage/MyPageMain.jsx
@@ -9,9 +9,9 @@ function MyPageMain() {
             <div className='MyInfoGrid'>
 
                 <div className="MyInfo">
-                    <h1>{localStorage.id} 안녕하세요!</h1>
+                    <h1>{localStorage.id}, 안녕하세요!</h1>
                     <div className="MyInfoSet">
-                        <a href=''>내 정보 수정</a>
+                        <button>수정 사항 변경</button>
                     </div>
                 </div>
 
@@ -21,24 +21,15 @@ function MyPageMain() {
                             <span>
                                 <h3>비밀번호</h3>
                             </span>
-                            <span>
-                                <a href="">0</a>
-                            </span>
                         </li>
                         <li>
                             <span>
                                 <h3>휴대폰번호</h3>
                             </span>
-                            <span>
-                                <a href="">0</a>
-                            </span>
                         </li>
                         <li>
                             <span>
                                 <h3>주소</h3>
-                            </span>
-                            <span>
-                                <a href="">0</a>
                             </span>
                         </li>
                     </ul>
