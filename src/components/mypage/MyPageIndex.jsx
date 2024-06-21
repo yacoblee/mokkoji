553ea36de74f7b7c40b5e0aabec50c82fc7a8718
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-21 13:07:54 +0900

    Update MyPageIndex.jsx

--- src/components/mypage/MyPageIndex.jsx
+++ src/components/mypage/MyPageIndex.jsx
@@ -1,6 +1,6 @@
 import '../../css/mypage/MyPageIndex.css';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MyPageGrid from './MyPageGrid';
