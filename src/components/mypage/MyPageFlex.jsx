8448a600959c39a3885839cf8800fafd70f7a366
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-21 17:43:35 +0900

    .

---
+++ src/components/mypage/MyPageFlex.jsx
@@ -0,0 +1,55 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faMobileScreenButton } from "@fortawesome/free-solid-svg-icons";
+import '../../css/mypage/MyPageMain.css';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function MyPageFlex() {
+    return (
+        <div className='MyInfoFlex'>
+
+            <div className="MyInfo">
+                <h1>(아이디), 안녕하세요!</h1>
+
+                <Link to='/mypage/set'>
+                    <div className="MyInfoSet">
+                        <button>내 정보 수정</button>
+                    </div>
+                </Link>
+
+            </div>
+
+            <div className="MyInfoBasic">
+                <div>
+                    <div>
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <span>
+                        (이름 나오는 곳)
+                    </span>
+                </div>
+                <div>
+                    <div>
+                        <FontAwesomeIcon icon={faMobileScreenButton} />
+                    </div>
+                    <span>
+                        (전화번호 나오는 곳)
+                    </span>
+                </div>
+                <div>
+                    <div>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </div>
+                    <span>
+                        (이메일 나오는 곳)
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MyPageFlex;
--- src/components/mypage/MyPageMain.jsx
+++ src/components/mypage/MyPageMain.jsx
@@ -2,42 +2,20 @@ import '../../css/mypage/MyPageMain.css';
 
 import React from 'react';
 
+import MyPageFlex from './MyPageFlex';
+import MyPageSet from './MyPageSet'
+import { Route, Routes } from 'react-router-dom';
+
 function MyPageMain() {
 
     return (
         <div className='MyPageMain'>
-            <div className='MyInfoGrid'>
-
-                <div className="MyInfo">
-                    <h1>{localStorage.id}, 안녕하세요!</h1>
-                    <div className="MyInfoSet">
-                        <button>수정 사항 변경</button>
-                    </div>
-                </div>
-
-                <div className="MyOrder">
-                    <ul>
-                        <li>
-                            <span>
-                                <h3>비밀번호</h3>
-                            </span>
-                        </li>
-                        <li>
-                            <span>
-                                <h3>휴대폰번호</h3>
-                            </span>
-                        </li>
-                        <li>
-                            <span>
-                                <h3>주소</h3>
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
+            <Routes>
+                <Route path='/' element={<MyPageFlex />} />
+                <Route path='/set' element={<MyPageSet />} />
+            </Routes>
         </div>
     )
 }
 
-export default React.memo(MyPageMain);
+export default MyPageMain;
