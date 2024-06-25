898ddcaad527dccd1b7d87aa4f557ce07d9a74ca
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-25 09:44:13 +0900

    .

--- src/components/mypage/mypagesub/MyPageLike.jsx
+++ src/components/mypage/mypagesub/MyPageLike.jsx
@@ -1,14 +1,53 @@
-import '../../../css/mypage/MyPageIndex.css';
+import '../../../css/mypage/subpage/MyPageLike.css';
 
 import React from 'react';
 
+import userInfo from '../../login/UserInforData';
+import GoodsItems from '../../product/ProductObject';
+
+
 function MyPageLike() {
+
+    const user = userInfo[0];   // 임의 지정
+    const item = GoodsItems;
+
+    const result = [];
+
+    function FindLikeItem() {
+        result = item.filter((goods) => {
+            if (user.mypage.isLike[0] === goods)
+                return goods;
+        })
+    }
+
+
     return (
+        <div className='MyLikeList'>
 
-        <div>
-            찜 목록
-        </div>
-    )
+            <div className='MyLikeHeader'>
+                <div>
+                    <input
+                        type="checkbox"
+                    // onChange={handleSelectAll}
+                    // checked={selectedProducts.length === products.length}
+                    />
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            {/* {user.mypage.isLike.forEach((isLikeNo) => {
+                let findGoods = item.find((goods) => isLikeNo === goods.id)
+                console.log(`출력됨${findGoods.id}`)
+            })} */}
+
+
+
+
+        </div >     // mylikelist
+
+    );       // return
+
 }
-
 export default MyPageLike;
