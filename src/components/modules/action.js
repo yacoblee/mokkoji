745de4cd73ceca190fd291312de852478d0485ce
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-26 12:12:23 +0900

    .

--- src/components/modules/action.js
+++ src/components/modules/action.js
@@ -12,10 +12,9 @@ export const logout = () => {
     sessionStorage.removeItem('userInfo');
     sessionStorage.removeItem('goodsList');
 
-    sessionStorage.removeItem('LoginSuccess');
     sessionStorage.removeItem('LoginId');
     sessionStorage.removeItem('LoginPw');
-    sessionStorage.removeItem('LoginUserInfo'); // 기훈- 로그아웃 이후 잔류데이터 삭제용 코드 4줄입니다...
+    sessionStorage.removeItem('LoginUserInfo'); // 기훈- 로그아웃 이후 잔류데이터 삭제용 코드 4줄입니다... 근데 LoginSuccess는 손봐야할듯요
 
     return {
         type: 'LOGOUT'
