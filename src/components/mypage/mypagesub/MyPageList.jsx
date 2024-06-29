6046ef71fec6e9d7860d728ecd6b9cdf886693aa
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-29 17:04:34 +0900

    List 진행중
    .

--- src/components/mypage/mypagesub/MyPageList.jsx
+++ src/components/mypage/mypagesub/MyPageList.jsx
@@ -5,7 +5,14 @@ function MyPageList() {
     const user = JSON.parse(sessionStorage.getItem("LoginUserInfo"))
     const items = JSON.parse(sessionStorage.getItem("goodsList"))
 
+    user.mypage.history.forEach(entry => {
+        entry.item = entry.item.map(itemId => {
+            let item = items.find(item => item.id === itemId);
+            return item ? item.id : null; // id가 없을 경우 null 처리
+        });
+    });
 
+    // console.log(user.mypage.history);
 
     return (
 
@@ -17,7 +24,9 @@ function MyPageList() {
                     <div className='MyListDate' key={list.date}>
                         <h2>{list.date}</h2>
                         <div className='MyListItem' key={list.item}>
-                            {list.item}
+                            <div className="MyCartPhoto">
+                                {/* <img src={items[list.item].productSrc[0]} /> */}
+                            </div>
                         </div>
                     </div>
                 )
