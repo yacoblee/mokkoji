277a2988d8b170a1de54221dabd8d795f40fc3db
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-06-29 16:31:40 +0900

    .

--- src/components/mypage/mypagesub/MyPageList.jsx
+++ src/components/mypage/mypagesub/MyPageList.jsx
@@ -1,10 +1,28 @@
 import '../../../css/mypage/MyPageIndex.css';
 
 function MyPageList() {
+
+    const user = JSON.parse(sessionStorage.getItem("LoginUserInfo"))
+    const items = JSON.parse(sessionStorage.getItem("goodsList"))
+
+
+
     return (
 
-        <div>
-            구매내역
+        <div className='MyPageList'>
+
+            {user.mypage.history.map((list) => {
+
+                return (
+                    <div className='MyListDate' key={list.date}>
+                        <h2>{list.date}</h2>
+                        <div className='MyListItem' key={list.item}>
+                            {list.item}
+                        </div>
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
