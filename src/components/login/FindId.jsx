5ca45d7a7eca48931160340e8b9e47f69ea28dfb
hyoyeun <141557384+hyoyeun@users.noreply.github.com>
2024-07-01 17:29:48 +0900

    [방효윤] 아이디찾기, 비밀번호 찾기 페이지 완료

--- src/components/login/FindId.jsx
+++ src/components/login/FindId.jsx
@@ -37,13 +37,102 @@ const FindId = () => {
             inputRef.current.focus();
         }
     }
+    //==============
+    const pRef = useRef(null)
+    const userFindIdInfo = useRef({
+        username: '',
+        PhoneNumber: ''
+    });
 
-    const [userInputName, setUserInputName] = useState('');
-    const [userInputPN, setUserInputPN] = useState('');
+    const userFindIdIdErrors = useRef({
+        username: '',
+        PhoneNumber: ''
+    });
 
-    const onChangeName = (e) => { setUserInputName(e.target.value) }
-    const onChangePN = (e) => { setUserInputPN(e.target.value) }
-    console.log(userInputName, userInputPN)
+    const [forceUpdater, forceUpdate] = useState(false);
+    const [alertShown, setAlertShown] = useState(false);// alket창 1번만 발생시키기 위한 함수 
+    const onChangeInputInfo = (e) => {
+        forceUpdate(!forceUpdater); // 랜더링 위한 임의의 콜백함수 
+        const { name, value } = e.target;
+        userFindIdInfo.current[name] = value;
+
+        if (name === 'username') {
+            const noneUserName = /^[가-힣]+$/;
+            const nameIsString = noneUserName.test(value);
+            if (nameIsString === false) {
+                inputNameRef.current.style.borderBottom = '1px solid red'
+                userFindIdIdErrors.current.username = false;
+            }
+            else {
+                inputNameRef.current.style.borderBottom = '1px solid #aaaaaa'
+                userFindIdIdErrors.current.username = true;
+            }
+        }
+
+
+
+        if (name === 'PhoneNumber') {
+            const noneNumber = /[^0-9]/g;
+            const isValidPhoneNumber = !noneNumber.test(value);
+            if (!isValidPhoneNumber) {
+                alert('-을 제외한 숫자만 입력해주세요');
+                inputPNRef.current.value = '';
+                userFindIdIdErrors.current.PhoneNumber = false;
+                setAlertShown(false);
+            }
+            return;
+        }
+        else {
+            userFindIdIdErrors.current.PhoneNumber = true;
+            setAlertShown(true);
+        }
+    }
+
+    const [findinguserinfo, setFindinguserinfo] = useState({
+        findingPw: '',
+        findingName: ''
+    });
+
+    console.log(userFindIdIdErrors);
+    const pRef2 = useRef(null);
+
+    const UserFindID = () => {
+        const allUserData = JSON.parse(localStorage.getItem('userInfo'));
+        if (!allUserData) {
+            return;
+        }
+
+        const isCheck = Object.values(userFindIdIdErrors.current).every(value => value === true);
+        if (isCheck) {
+            const userExistsName = allUserData.find(it => it.name === userFindIdInfo.current.username);
+            const userExistsPhoneNumber = allUserData.find(it => it.phoneNumber === userFindIdInfo.current.PhoneNumber);
+
+            if (userExistsName && userExistsPhoneNumber && userExistsName.name === userExistsPhoneNumber.name) {
+                setFindinguserinfo({
+                    findingName: userExistsName.name,
+                    findingId: userExistsName.id
+                });
+                pRef.current.style.visibility = 'visible'
+                pRef2.current.style.visibility = 'visible'
+
+            } else {
+                pRef.current.style.visibility = 'hidden'
+                pRef2.current.style.visibility='hidden'
+                alert('⚠️ 입력하신 정보와 동일한 아이디가 존재하지 않습니다.')
+            }
+        } else {
+            alert('⚠️ 조건에 맞게 정보를 다시입력해주세요')
+            inputPNRef.current.value = '';
+            inputNameRef.current.value = '';
+            inputNameRef.current.style.borderBottom = '1px solid #aaaaaa';
+            inputPNRef.current.style.borderBottom = '1px solid #aaaaaa';
+            MoveToInLabel(labelPNRed, inputPNRef)
+            MoveToInLabel(labeNameRed, inputNameRef)
+        }
+    };
+
+
+
     return (
         <div>
             <div className="findId-body">
@@ -55,11 +144,11 @@ const FindId = () => {
                         <div className="findId-textarea">
                             <ul>
                                 <Link to={'/'}><li>홈 &gt;</li></Link>
-                                <Link to={'/'}><li>로그인 &gt;</li></Link>
-                                <Link to={'/'}><li>아이디 찾기</li></Link>
+                                <Link to={'/Login'}><li>로그인 &gt;</li></Link>
+                                <Link to={'/Login/FindId'}><li>아이디 찾기</li></Link>
                             </ul>
                             <div className="findId-imgBox"><Link to='/'><img src="/images/main/main1.png" alt="로고이미지" /></Link></div>
-                            <h4>Finding ID</h4>
+                            <h4>아이디를 찾기 위해 아래 개인 정보를 입력해주세요</h4>
 
                             <div className="findId-Box">
                                 <div className="findId-inputArea">
@@ -69,9 +158,9 @@ const FindId = () => {
                                     >이름</label>
                                     <input type="text"
                                         maxLength={4}
-                               
+                                        name="username"
                                         ref={inputNameRef}
-                                        onChange={onChangeName}
+                                        onChange={onChangeInputInfo}
                                         onFocus={() => MoveToOutLabel(labeNameRed)}
                                         onBlur={() => MoveToInLabel(labeNameRed, inputNameRef)} />
                                 </div>
@@ -82,16 +171,19 @@ const FindId = () => {
                                     >핸드폰 번호</label>
                                     <input type="text"
                                         ref={inputPNRef}
+                                        name='PhoneNumber'
                                         maxLength={11}
-                                        onChange={onChangePN}
+                                        onChange={onChangeInputInfo}
                                         onFocus={() => MoveToOutLabel(labelPNRed)}
                                         onBlur={() => MoveToInLabel(labelPNRed, inputPNRef)} />
                                 </div>
                                 <div>
-                                    <p>{userInfo.name}님의 아이디는 : {userInfo.id}입니다.</p>
+                                    <p className="ptag" ref={pRef}><span className="showname">{findinguserinfo.findingName}</span>님의 아이디는 : <span className="showname">{findinguserinfo.findingId}</span>입니다.</p>
+                                    <p  ref={pRef2} className="goLogin"><Link to={'/Login'}><span className="find-span">
+                                    로그인 페이지로 돌아가기</span></Link></p>
                                 </div>
 
-                                <button>아이디 찾기</button>
+                                <button onClick={UserFindID}>Finding ID</button>
                             </div>
 
                         </div>
