08ca64a5006ca965d7941d9db5406dcfd73b4ec5
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-07-01 21:05:15 +0900

    예약 페이지 수정

--- src/components/Reserve/ReservationDetail.jsx
+++ src/components/Reserve/ReservationDetail.jsx
@@ -1,4 +1,4 @@
-
+import Reservesource from "./ReserveSource";
 
 
 const ReservationDeatil = () => {
@@ -8,11 +8,10 @@ const ReservationDeatil = () => {
     return (
         <div className="reservation_detail_inner">
 
-            <div className="detail_inner_head">
-                <h2>상세정보</h2>
-            </div>
-
             <div className="detail_inner_ul1">
+                <div className="detail_inner_head">
+                    <h2>상세정보</h2>
+                </div>
                 <div>
                     <h4>패키지 상세정보</h4>
                     <ul>
@@ -25,9 +24,29 @@ const ReservationDeatil = () => {
                         <li>음주 상태의 참여자는 서비스 이용이 거부될 수 있습니다. 이 경우 환불은 불가합니다.</li>
                     </ul>
                 </div>
+                <div>
+                    <h4>예약 제한사항</h4>
+                    <ul>
+                        <li>그룹 규모: 참여자 수 1-25인.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>편의성/접근성</h4>
+                    <ul>
+                        <li>유모차 및 휠체어 이용이 불가합니다.</li>
+                    </ul>
+                </div>
             </div>
-            <div className="detail_inner_ul2">
 
+            <div className="detail_inner_img">
+                <div className="detail_inner_img_head">
+                    <h2>상세 이미지</h2>
+                </div>
+                <div  >
+                    {Reservesource.flatMap(item => item.productSrc).map((src, i) => (
+                        <img key={i} className="" src={src} alt={`slide ${i}`} />
+                    ))}
+                </div>
             </div>
 
         </div>
