e23a4a3413edd5a4d7cadff267ca85a9e4f8b47e
hyoyeun <141557384+hyoyeun@users.noreply.github.com>
2024-07-10 00:04:09 +0900

    [방효윤] 라벨 인풋 연결

--- src/components/login/FindId.jsx
+++ src/components/login/FindId.jsx
@@ -117,7 +117,7 @@ const FindId = () => {
             } else {
                 pRef.current.style.visibility = 'hidden'
                 pRef2.current.style.visibility='hidden'
-                alert('⚠️ 입력한 정보와 일치하는 회원정보가 존재하지 않습니다.')
+                alert('⚠️ 입력하신 정보와 일치하는 회원 정보를 찾을 수 없습니다.')
             }
         } else {
             alert('⚠️ 조건에 맞게 정보를 다시입력해주세요')
