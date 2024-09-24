a17242f3f851f418840fb7668e53d4ff1b3f6228
hyoyeun <141557384+hyoyeun@users.noreply.github.com>
2024-09-24 12:55:17 +0900

    sql 구문 변경 - user uk, AUTO_INCREMENT로 변경
    ''ALTER TABLE `users`
    MODIFY COLUMN `user_sequence` INT NOT NULL AUTO_INCREMENT COMMENT '인덱스',
    ADD UNIQUE (`user_sequence`); uk, AUTO_INCREMENT 로 변경

--- mokkoji_backend/src/main/resources/mySQL/insertproduct.sql
+++ mokkoji_backend/src/main/resources/mySQL/insertproduct.sql
@@ -562,3 +562,8 @@ INSERT INTO productoptions(product_id, option_content,option_price)
 VALUES (19, '중',40000);
 INSERT INTO productoptions(product_id, option_content,option_price)
 VALUES (19, '대',90000);
+
+
+ALTER TABLE `users`
+MODIFY COLUMN `user_sequence` INT NOT NULL AUTO_INCREMENT COMMENT '인덱스',
+ADD UNIQUE (`user_sequence`);
