e6d2414e300e4531d1bdadd5f229d93e8f2fad26
hyemiJ <166362402+hyemiJ@users.noreply.github.com>
2024-09-25 09:35:52 +0900

    repository 추가

--- mokkoji_backend/src/main/java/com/example/mokkoji_backend/repository/ProductsRepository.java
+++ mokkoji_backend/src/main/java/com/example/mokkoji_backend/repository/ProductsRepository.java
@@ -1,8 +1,10 @@
 package com.example.mokkoji_backend.repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,18 +14,19 @@ import com.example.mokkoji_backend.domain.ProductsDTO;
 import com.example.mokkoji_backend.entity.goods.Products;
 
 public interface ProductsRepository extends JpaRepository<Products, Long>{
-	//@EntityGraph(attributePaths = {"options"})
+	
+	@EntityGraph(attributePaths = {"options"})
+	List<Products> findAll();
+	
 	@Query("SELECT new com.example.mokkoji_backend.domain.ProductsDTO"
 			+ "(p.id, p.name, p.price, p.mainImageName, p.categoryId,p.guide) FROM Products p where p.categoryId=:categoryId")
 	List<ProductsDTO> findByCategoryId(@Param("categoryId") String categoryId);
 	
-	//@EntityGraph(attributePaths = {"options"})
 	@Query("SELECT new com.example.mokkoji_backend.domain.ProductsDTO"
 			+ "(p.id, p.name, p.price, p.mainImageName, p.categoryId) FROM Products p")
 	List<ProductsDTO> findList();
 	
-	@EntityGraph(attributePaths = {"options"})
-	List<Products> findAll();
+	Page<ProductsDTO> findByCategoryIdAndNameContaining(String categoryId , String name , Pageable pageable);
 	
 	@EntityGraph(attributePaths = {"options"})
 	Optional<Products> findById(Long id);
