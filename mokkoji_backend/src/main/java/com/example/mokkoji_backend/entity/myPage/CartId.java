be798a6e2766d90cb7a16eb94280807fdd775233
hyemiJ <166362402+hyemiJ@users.noreply.github.com>
2024-09-25 10:17:41 +0900

    Create CartId.java

---
+++ mokkoji_backend/src/main/java/com/example/mokkoji_backend/entity/myPage/CartId.java
@@ -0,0 +1,21 @@
+package com.example.mokkoji_backend.entity.myPage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userId;
+	private int productId;
+	private String optionContent;
+	private String packagingOptionContent;
+
+}
