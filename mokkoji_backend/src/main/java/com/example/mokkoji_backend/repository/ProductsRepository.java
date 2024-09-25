e9427f453a76ddda5cb09a14bb8f00a4c0bf21d7
hyemiJ <166362402+hyemiJ@users.noreply.github.com>
2024-09-25 10:00:32 +0900

    Update ProductsRepository.java

--- mokkoji_backend/src/main/java/com/example/mokkoji_backend/repository/ProductsRepository.java
+++ mokkoji_backend/src/main/java/com/example/mokkoji_backend/repository/ProductsRepository.java
@@ -1,10 +1,10 @@
 package com.example.mokkoji_backend.repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
