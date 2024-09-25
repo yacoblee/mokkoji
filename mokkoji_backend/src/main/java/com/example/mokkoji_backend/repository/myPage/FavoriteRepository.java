62ae5e5dafa6ba1d114eefad41031cd33b846065
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-09-25 13:01:00 +0900

    .
    .

--- mokkoji_backend/src/main/java/com/example/mokkoji_backend/repository/myPage/FavoriteRepository.java
+++ mokkoji_backend/src/main/java/com/example/mokkoji_backend/repository/myPage/FavoriteRepository.java
@@ -1,12 +1,11 @@
 package com.example.mokkoji_backend.repository.myPage;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.mokkoji_backend.entity.myPage.Favorite;
 import com.example.mokkoji_backend.entity.myPage.FavoriteId;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
