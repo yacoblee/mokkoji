dd340167bcb4e7e27bea725e66facd56f33837cd
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-10-07 18:17:34 +0900

    .
    .

--- mokkoji_frontend/src/components/mypage/MyPageAddress.jsx
+++ mokkoji_frontend/src/components/mypage/MyPageAddress.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import '../../css/mypage/subpage/MyPageAddress.css';
+
 function MyPageAddress({ userMain, userAddress, myPageAddress }) {
 
     useEffect(() => {
@@ -8,11 +10,37 @@ function MyPageAddress({ userMain, userAddress, myPageAddress }) {
 
     return (
         <>
-            <div>
-
-                Juso
-
-            </div>
+            {userAddress.map((address) => {
+                return (
+                    <table className="AddressListTable" key={address.isDefault}>
+                        <tr>
+                            <th className="AddressNameCell" rowspan="2">
+                                <input type="radio" name="defaultAddress" />
+                                <span>&nbsp;{address.locationName}</span>
+                            </th>
+                            <th>&nbsp;</th>
+                            <td style={{ width: '100px' }} rowspan="4">
+                                <button className="MyInfoSetting">주소지 수정</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>[{address.postalCode}]</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <span>&nbsp;수취인 : {address.recipientName !== null ? address.recipientName : "미기입"}</span>
+                            </th>
+                            <td className="MainAddressCell">{address.streetAddress}</td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <span>&nbsp;연락처 : {address.recipientPhone !== null ? address.recipientPhone : "미기입"}</span>
+                            </th>
+                            <td>{address.detailedAddress}</td>
+                        </tr>
+                    </table>
+                )
+            })}
         </>
     )
 }
