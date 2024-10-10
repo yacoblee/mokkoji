00e7c3de52ff0282c1ee86b898d4e2923437edd8
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-10-11 00:37:30 +0900

    ㅇ

--- mokkoji_frontend/src/components/administrator/UserManager.jsx
+++ mokkoji_frontend/src/components/administrator/UserManager.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import '../../css/administrator/adminUsers.css';
+import '../../css/administrator/adminUsers.css';
 import { apiCall } from '../../service/apiService';
 
 
