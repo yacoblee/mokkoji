ba28b167d0c281634ed0bbc0b606ca5ae329a42a
e-MKH <167267400+e-MKH@users.noreply.github.com>
2024-10-21 12:03:14 +0900

    .
    .

--- mokkoji_backend/src/main/resources/mySQL/Update.sql
+++ mokkoji_backend/src/main/resources/mySQL/Update.sql
@@ -1,6 +1,6 @@
-UPDATE project.code
+UPDATE code
 SET main_type_name='상품 상태', sub_type_name='판매중'
 WHERE main_type='03PS' AND sub_type='0';
-UPDATE project.code
+UPDATE code
 SET main_type_name='상품 상태', sub_type_name='준비중'
 WHERE main_type='03PS' AND sub_type='1';
